30684b9f02f6df180b03290fc69fce05a21e4780
Gianni GIUDICE <gianni.giudice@lacatholille.fr>
2021-10-12 09:55:32 +0200

    :sparkles: Ajout Tests (absent lors du cours)

---
+++ tests/Fibonacci.Tests/UnitTest1.cs
@@ -0,0 +1,26 @@
+using System;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Xunit;
+
+namespace Fibonacci.Tests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public async Task Test1()
+        {
+            var builder = new DbContextOptionsBuilder<FibonacciDataContext>();
+            var dataBaseName = Guid.NewGuid().ToString();
+            builder.UseInMemoryDatabase(dataBaseName);
+
+            var options = builder.Options;
+            var fibonacciDataContext = new FibonacciDataContext(options);
+            await fibonacciDataContext.Database.EnsureCreatedAsync(); 
+            
+            var result = await new Compute(fibonacciDataContext).ExecuteAsync(new[] {"4"});
+            Assert.Single(result);
+            Assert.Equal(3, result[0]);
+        }
+    }
+}
