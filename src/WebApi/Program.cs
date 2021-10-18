8a47e151214dd3b5dd391d1590fa935a67236c26
Gianni GIUDICE <gianni.giudice@lacatholille.fr>
2021-10-18 17:28:21 +0200

    :sparkles: Dernier cours

---
+++ src/WebApi/Program.cs
@@ -0,0 +1,41 @@
+using System;
+using Fibonacci;
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+builder.Services.AddDbContext<FibonacciDataContext>(options => 
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+builder.Services.AddScoped<Compute>();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.Use(async (context, next) => {
+    Console.WriteLine("youhou");
+    // Do work that doesn't write to the Response. 
+    await next.Invoke(); 
+    // Do logging or other work that doesn't write to the Response. 
+}); 
+
+
+app.MapControllers();
+
+app.Run();
+app.Run();
