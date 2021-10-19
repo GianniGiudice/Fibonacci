93b474f1146b1ed6a5e2fceb0ad6bdbd037005c7
Gianni GIUDICE <gianni.giudice@lacatholille.fr>
2021-10-19 15:57:26 +0200

    :sparkles: API Fonctionnelle

--- src/WebApi/Program.cs
+++ src/WebApi/Program.cs
@@ -1,6 +1,9 @@
-using System;
 using Fibonacci;
+using Microsoft.AspNetCore.Builder;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -27,15 +30,6 @@ app.UseHttpsRedirection();
 
 app.UseAuthorization();
 
-app.Use(async (context, next) => {
-    Console.WriteLine("youhou");
-    // Do work that doesn't write to the Response. 
-    await next.Invoke(); 
-    // Do logging or other work that doesn't write to the Response. 
-}); 
-
-
 app.MapControllers();
 
-app.Run();
 app.Run();
